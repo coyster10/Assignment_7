1284133534471e6951447c8c70f42328e52d0dd9
coyster10 <id6145ic@go.minnstate.edu>
2021-11-14 17:50:05 -0600

    Update

---
+++ Models/Course.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const schema = mongoose.Schema;
+
+const COURSE = new mongoose.Schema({
+    courseID:
+    {
+        type: String,
+        required: true
+    },
+    courseName:
+    {
+        type: String,
+        required: true
+    },
+    courseInstructor:
+    {
+        type: String,
+        required: true
+    },
+    courseCredits:
+    {
+        type: Number,
+        required: true
+    },
+    dateEntered:
+    {
+        type: Date,
+        required: true,
+        default: Date.now
+    },
+});
+mongoose.model("COURSE", COURSE);
